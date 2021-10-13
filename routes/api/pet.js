1ff062106a3618e1f9bdaab8028d4a82d21f7c65
Paul Smith <prsmith@ranken.edu>
2021-10-13 09:20:26 -0500

    search interface example

--- routes/api/pet.js
+++ routes/api/pet.js
@@ -3,7 +3,7 @@ const debugError = require('debug')('app:error');
 const express = require('express');
 const { nanoid } = require('nanoid');
 const dbModule = require('../../database');
-const { newId } = require('../../database');
+const { newId, connect } = require('../../database');
 const Joi = require('joi');
 const validId = require('../../middleware/validId');
 const validBody = require('../../middleware/validBody');
@@ -40,9 +40,68 @@ const router = express.Router();
 // define routes
 router.get('/list', async (req, res, next) => {
   try {
-    debug('get all pets');
-    const pets = await dbModule.findAllPets();
-    res.json(pets);
+    // get inputs
+    let { keywords, species, minAge, maxAge, sortBy, pageNumber, pageSize } =
+      req.query;
+    debug(req.query);
+
+    // match stage
+    const match = {};
+    if (keywords) {
+      match.$text = { $search: keywords };
+    }
+    if (species) {
+      match.species = { $eq: species };
+    }
+
+    minAge = parseInt(minAge);
+    maxAge = parseInt(maxAge);
+    if (minAge && maxAge) {
+      match.age = { $gte: minAge, $lte: maxAge };
+    } else if (minAge) {
+      match.age = { $gte: minAge };
+    } else if (maxAge) {
+      match.age = { $lte: maxAge };
+    }
+
+    // sort stage
+    let sort = { name: 1, createdDate: 1 };
+    switch (sortBy) {
+      case 'species': sort = { species: 1, name: 1, createdDate: 1 }; break;
+      case 'species_desc': sort = { species: -1, name: -1, createdDate: -1 }; break;
+      case 'name': sort = { name: 1, createdDate: 1 }; break;
+      case 'name_desc': sort = { name: -1, createdDate: -1 }; break;
+      case 'age': sort = { age: 1, createdDate: 1 }; break;
+      case 'age_desc': sort = { age: -1, createdDate: -1 }; break;
+      case 'gender': sort = { gender: 1, name: 1, createdDate: 1 }; break;
+      case 'gender_desc': sort = { gender: -1, name: -1, createdDate: -1 }; break;
+      case 'newest': sort = { createdDate: -1 }; break;
+      case 'oldest': sort = { createdDate: 1 }; break;
+    }
+
+    // project stage
+    const project = { species: 1, name: 1, age: 1, gender: 1 };
+
+    // skip & limit stages
+    pageNumber = parseInt(pageNumber) || 1;
+    pageSize = parseInt(pageSize) || 5;
+    const skip = (pageNumber - 1) * pageSize;
+    const limit = pageSize;
+
+    // pipeline
+    const pipeline = [
+      { $match: match },
+      { $sort: sort },
+      { $project: project },
+      { $skip: skip },
+      { $limit: limit },
+    ];
+
+    const db = await connect();
+    const cursor = db.collection('pets').aggregate(pipeline);
+    const results = await cursor.toArray();
+
+    res.json(results);
   } catch (err) {
     next(err);
   }
@@ -72,23 +131,28 @@ router.put('/new', validBody(newPetSchema), async (req, res, next) => {
     next(err);
   }
 });
-router.put('/:petId', validId('petId'), validBody(updatePetSchema), async (req, res, next) => {
-  try {
-    const petId = req.petId;
-    const update = req.body;
-    debug(`update pet ${petId}`, update);
+router.put(
+  '/:petId',
+  validId('petId'),
+  validBody(updatePetSchema),
+  async (req, res, next) => {
+    try {
+      const petId = req.petId;
+      const update = req.body;
+      debug(`update pet ${petId}`, update);
 
-    const pet = await dbModule.findPetById(petId);
-    if (!pet) {
-      res.status(404).json({ error: `Pet ${petId} not found.` });
-    } else {
-      await dbModule.updateOnePet(petId, update);
-      res.json({ message: `Pet ${petId} updated.` });
+      const pet = await dbModule.findPetById(petId);
+      if (!pet) {
+        res.status(404).json({ error: `Pet ${petId} not found.` });
+      } else {
+        await dbModule.updateOnePet(petId, update);
+        res.json({ message: `Pet ${petId} updated.` });
+      }
+    } catch (err) {
+      next(err);
     }
-  } catch (err) {
-    next(err);
   }
-});
+);
 router.delete('/:petId', validId('petId'), async (req, res, next) => {
   try {
     const petId = req.petId;
