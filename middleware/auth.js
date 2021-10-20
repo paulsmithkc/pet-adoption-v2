df9f7040bdbf01009c0add5c83d0cc552301bde2
Paul Smith <prsmith@ranken.edu>
2021-10-20 16:15:23 -0500

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,18 +2,33 @@ const debug = require('debug')('app:middleware:auth');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
+const authSecret = config.get('auth.secret');
+
 const auth = () => {
   return (req, res, next) => {
-    try {
-      const authCookie = req.cookies.authToken;
-      if (authCookie) {
-        const authSecret = config.get('auth.secret');
-        debug(authCookie, authSecret);
-        const authPayload = jwt.verify(authCookie, authSecret);
-        req.auth = authPayload;
-      }
-    } catch (err) {
-      debug(err);
+    const authHeader = req.headers['authorization'];
+    const authCookie = req.cookies.authToken;
+
+    if (authHeader) {
+      const [authType, authToken] = authHeader.split(' ', 2);
+      if (authType === 'Bearer' && authToken) {
+        try {
+          req.auth = jwt.verify(authToken, authSecret);
+        } catch (err) {
+          debug('invalid token');
+        }
+      }    
+    } else if (authCookie) {
+      try {
+        req.auth = jwt.verify(authCookie, authSecret);
+        const cookieOptions = {
+          httpOnly: true,
+          maxAge: parseInt(config.get('auth.cookieMaxAge')),
+        };
+        res.cookie('authToken', authToken, cookieOptions);
+      } catch (err) {
+        debug('invalid token');
+      }    
     }
 
     next();
